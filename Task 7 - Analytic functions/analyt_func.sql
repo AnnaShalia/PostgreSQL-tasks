5015824efd516cdbe8f20a84ede119b9d58af086
AnnaShalia <anna.lotica@gmail.com>
2022-02-22 11:18:58 +0200

    Task 7. Analytics functions + upd. formatting

--- Task 7 - Analytic functions/analyt_func.sql
+++ Task 7 - Analytic functions/analyt_func.sql
@@ -5,18 +5,18 @@
 	Production.Product */
 
 WITH orders AS (
-	SELECT od.productid, SUM(od.linetotal) sumtotal
-	  FROM sales.salesorderheader o
+    SELECT od.productid, SUM(od.linetotal) sumtotal
+      FROM sales.salesorderheader o
       JOIN sales.salesorderdetail od
         ON o.salesorderid = od.salesorderid
      WHERE to_char(o.orderdate, 'yyyy-mm') = '2013-01'
      GROUP BY od.productid
 	), 
 	ranked_items AS (
-	SELECT p.name product, o.sumtotal, PERCENT_RANK() OVER(ORDER BY o.sumtotal) perrank
-	  FROM production.product p
-	  JOIN orders o
-		ON o.productid = p.productid
+    SELECT p.name product, o.sumtotal, PERCENT_RANK() OVER(ORDER BY o.sumtotal) perrank
+      FROM production.product p
+      JOIN orders o
+        ON o.productid = p.productid
 	)
 SELECT product, sumtotal
   FROM ranked_items
@@ -50,28 +50,28 @@ SELECT DISTINCT(listprice)
 	Production.ProductCategory */
 
 WITH categories AS(
-		SELECT c.productcategoryid category, c.name, sc.productsubcategoryid 
-		  FROM production.productcategory c
-		  JOIN production.productsubcategory sc
-			ON c.productcategoryid = sc.productcategoryid
+        SELECT c.productcategoryid category, c.name, sc.productsubcategoryid 
+          FROM production.productcategory c
+          JOIN production.productsubcategory sc
+            ON c.productcategoryid = sc.productcategoryid
 	), 
 	sales AS(
-		SELECT od.productid, od.linetotal, o.orderdate
-		  FROM sales.salesorderdetail od
-		  JOIN sales.salesorderheader o
-			ON od.salesorderid = o.salesorderid
+	SELECT od.productid, od.linetotal, o.orderdate
+	  FROM sales.salesorderdetail od
+          JOIN sales.salesorderheader o
+            ON od.salesorderid = o.salesorderid
 	), 
 	categories_sales AS (
-		SELECT c.name, SUM(s.linetotal) sales, EXTRACT(YEAR from s.orderdate) order_year
-	      FROM categories c
-	      JOIN production.product p
-		    ON c.productsubcategoryid = p.productsubcategoryid
-	      JOIN sales s
-		    ON s.productid = p.productid
-	  GROUP BY c.name, order_year
+        SELECT c.name, SUM(s.linetotal) sales, EXTRACT(YEAR from s.orderdate) order_year
+          FROM categories c
+          JOIN production.product p
+            ON c.productsubcategoryid = p.productsubcategoryid
+          JOIN sales s
+            ON s.productid = p.productid
+	 GROUP BY c.name, order_year
 	),
 	sales_yoy AS (
-		SELECT name category, sales, order_year,(sales - LAG(sales) OVER(
+        SELECT name category, sales, order_year,(sales - LAG(sales) OVER(
 			PARTITION BY name ORDER BY order_year)) / sales yoy
           FROM categories_sales
 	)
@@ -86,10 +86,10 @@ SELECT category, sales, yoy
 WITH sales AS(
     SELECT o.orderdate, MAX(od.linetotal) total
       FROM sales.salesorderheader o
-	  JOIN sales.salesorderdetail od
-	    ON o.salesorderid = od.salesorderid
-	 WHERE to_char(o.orderdate, 'yyyy-mm')= '2013-01'
-	GROUP BY o.orderdate
+      JOIN sales.salesorderdetail od
+        ON o.salesorderid = od.salesorderid
+     WHERE to_char(o.orderdate, 'yyyy-mm')= '2013-01'
+     GROUP BY o.orderdate
 	)
 SELECT orderdate, MAX(total) OVER(PARTITION BY orderdate) maxorder
   FROM sales;
@@ -101,8 +101,8 @@ Sales.SalesOrderHeader
 	Production.ProductSubcategory */
 
 WITH sold_products AS (
-	SELECT od.productid
-	  FROM sales.salesorderheader o
+    SELECT od.productid
+      FROM sales.salesorderheader o
       JOIN sales.salesorderdetail od
         ON o.salesorderid = od.salesorderid
      WHERE to_char(o.orderdate, 'yyyy-mm')= '2013-01'
@@ -112,12 +112,12 @@ WITH sold_products AS (
 		PARTITION BY s.name ORDER BY COUNT(p.name) DESC) AS rnk 
       FROM production.productsubcategory s
       JOIN production.product p
-	    ON s.productsubcategoryid = p.productsubcategoryid
-	  JOIN sold_products sp
-		ON sp.productid = p.productid
-	GROUP BY s.name, mostfreq
+        ON s.productsubcategoryid = p.productsubcategoryid
+      JOIN sold_products sp
+        ON sp.productid = p.productid
+     GROUP BY s.name, mostfreq
 	)
 SELECT name, mostfreq 
   FROM sold_product_subcategories
  WHERE rnk = 1
-ORDER BY mostfreq;
+ ORDER BY mostfreq;
